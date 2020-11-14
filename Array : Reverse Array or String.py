9730d7e4653e3dff0994791386c80f7e3808f7df
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-11-14 23:36:16 +0530

    Create Array : Reverse Array or String.py

---
+++ Array : Reverse Array or String.py
@@ -0,0 +1,9 @@
+#ques :https://leetcode.com/problems/reverse-string/
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            s[i],s[-1-i]=s[-1-i],s[i]
+        return s
