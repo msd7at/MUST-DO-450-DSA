6bff9575a7839981601236107b989503b8cc3c96
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-11-21 22:16:55 +0530

    Create 021 Array : Subarray Sum Equals K.py

---
+++ 021 Array : Subarray Sum Equals K.py
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/subarray-sum-equals-k/
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        d={0:1}
+        ans=0
+        s=0
+        for i in nums:
+            s+=i
+            if s-k in d:
+                ans+=d[s-k]
+            if s in d:
+                d[s]+=1
+            else:
+                d[s]=1
+        return ans
+                
