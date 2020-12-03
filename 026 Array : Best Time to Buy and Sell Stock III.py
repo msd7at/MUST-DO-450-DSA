1144845fd5fc25ec35df5d9515fb7e4da65fbd3c
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-12-03 19:13:34 +0530

    Update 026 Array : Best Time to Buy and Sell Stock III.py

--- 026 Array : Best Time to Buy and Sell Stock III.py
+++ 026 Array : Best Time to Buy and Sell Stock III.py
@@ -3,6 +3,31 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if prices==[]:
             return 0
+        n=len(prices)
+        l=[0]*n
+        r=[0]*n
+        pmi=0
+        mi=prices[0]
+        for i in range(1,n):
+            mi=min(mi,prices[i])
+            pmi=max(pmi,prices[i]-mi)
+            l[i]=pmi
+        ma=prices[-1]
+        pma=0
+        for i in range(n-2,-1,-1):
+            ma=max(prices[i],ma)
+            pma=max(pma,ma-prices[i])
+            r[i]=pma
+        ans=max(l)
+        for i in range(n-1):
+            ans=max(ans,l[i]+r[i+1])
+        return ans
+    
+    
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if prices==[]:
+            return 0
         cmin=prices[0]
         lspp=[]
         mapt=0
