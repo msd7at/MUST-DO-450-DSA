cb7bfda41a545505f0d7d65e67d61d3d80acd494
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-12-19 20:29:15 +0530

    Update 058 String : Split the binary string into substrings with equal number of 0s and 1s.py

--- 058 String : Split the binary string into substrings with equal number of 0s and 1s.py
+++ 058 String : Split the binary string into substrings with equal number of 0s and 1s.py
@@ -1,4 +1,20 @@
 
+s=input()
+ans=0
+c=0
+for i in s:
+    if i=="0":
+        c-=1
+    else:
+        c+=1
+    if c==0:
+        ans+=1
+print(ans)
+
+
+
+
+
 def maxSubStr(str, n): 
 	count0 = 0
 	count1 = 0
