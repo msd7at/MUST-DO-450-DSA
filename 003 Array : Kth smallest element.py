4470b7f81a1fa9978f25c1ecdf155b897a3ce5c5
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2021-01-04 23:23:05 +0530

    Update 003 Array : Kth smallest element.py

--- 003 Array : Kth smallest element.py
+++ 003 Array : Kth smallest element.py
@@ -4,7 +4,7 @@ def kthSmallest(arr, l, r, k):
     arr : given array
     l : starting index of the array i.e 0
     r : ending index of the array i.e size-1
-    k : find kth smallest element and return using this function
+    k : find k  th smallest element and return using this function
     '''
     arr.sort()
     return arr[k-1]
